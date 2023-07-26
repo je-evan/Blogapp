05c926d029140e35982d259f2c0008be9201fdf6
je-evan <dhakaljeevan165@gmail.com>
2023-07-26 22:35:23 +0545

    refactor: post list pagination

--- post/views.py
+++ post/views.py
@@ -8,7 +8,7 @@ from django.db.models import Q
 
 class PostList(generic.ListView):
     template_name = 'post_list.html'
-    paginate_by = 3
+    paginate_by = 5
     
     def get_queryset(self):
         search = self.request.GET.get('search', '')
