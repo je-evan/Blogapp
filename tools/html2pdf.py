140e653c67d5f19ea7311b0e89796b0e1488ea88
je-evan <dhakaljeevan165@gmail.com>
2023-08-04 21:16:25 +0545

    feat: margin on pdf file

--- tools/html2pdf.py
+++ tools/html2pdf.py
@@ -6,10 +6,17 @@ from os import remove
 
 
 def html2pdf(html_file):
+    options = {
+        'page-size': 'A4',
+        'margin-top': '0.75in',
+        'margin-right': '0.75in',
+        'margin-bottom': '0.75in',
+        'margin-left': '0.75in',
+    }
     FileSystemStorage(location=settings.MEDIA_ROOT).save(html_file.name, html_file)
     input_file = join(settings.MEDIA_ROOT,html_file.name)
     output_file = join(settings.MEDIA_ROOT, splitext(html_file.name)[0]  + ".pdf")
     with open(input_file) as file:
-        from_file(file, output_file)
+        from_file(file, output_file, options=options)
     remove(input_file)
     return output_file
